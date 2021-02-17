2b866228a63fbecb03fd52db76e0a61231bb8be7
DefaultID <arteam_101@mail.ru>
2021-02-17 20:40:34 +0700

    Formatted

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 using namespace std;
 vector<int> r;
 vector<int> x;
@@ -10,24 +10,29 @@ vector<float> per;
 
 int main()
 {
-
+    cout << "USE FORMAT UNDER LINE\n---------------\n figure(x.y,r)\n";
     while (1) {
         bool InputCorrect = 0;
-        cout << "USE FORMAT UNDER LINE\n---------------\n figure(x.y,r)\n";
-       while(!InputCorrect){
-           string input;
-           cin >> input;
-           if("circle" == input.substr(0 , input.find_first_of("("))) {
-               int xx = atoi(input.substr(input.find_first_of("(") + 1, input.find_first_of(" ") - 1).c_str());
-               x.push_back(xx);
-               int yy = atoi(input.substr(input.find_first_of(".") + 1, input.find_first_of(",") - 1).c_str());
-               y.push_back(yy);
-                int rad = atoi(input.substr(input.find_first_of(",") + 1, input.find_last_of(")") - 1).c_str());
-                r.push_back(rad);
-           }
-           else continue;
-       }
 
+        while (!InputCorrect) {
+            string input;
+            cin >> input;
+            if ("circle" == input.substr(0, input.find_first_of("("))) {
+                int xx = atoi(input.substr(input.find_first_of("(") + 1,
+                                           input.find_first_of(" ") - 1)
+                                      .c_str());
+                x.push_back(xx);
+                int yy = atoi(input.substr(input.find_first_of(".") + 1,
+                                           input.find_first_of(",") - 1)
+                                      .c_str());
+                y.push_back(yy);
+                int rad = atoi(input.substr(input.find_first_of(",") + 1,
+                                            input.find_last_of(")") - 1)
+                                       .c_str());
+                r.push_back(rad);
+            } else {
+                cout << "WRONG FORMAT";
+            }
+        }
+        return 0;
     }
-    return 0;
-}

3f12ca65c618500b7109f275f251cef2cf5aa708
DefaultID <arteam_101@mail.ru>
2021-02-21 14:41:48 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,31 +8,72 @@ vector<int> y;
 vector<float> s;
 vector<float> per;
 
+bool isFormatCorrect(string str)
+{
+    if (str.empty())
+        return 0;
+
+    int k = 0;
+    for (auto i : str)
+        if (i == '.')
+            k++;
+    if (k != 1)
+        return 0;
+
+    k = 0;
+    for (auto i : str)
+        if (i == ',')
+            k++;
+    if (k != 1)
+        return 0;
+
+    k = 0;
+    for (auto i : str)
+        if (i == '(')
+            k++;
+    if (k != 1)
+        return 0;
+
+    k = 0;
+    for (auto i : str)
+        if (i == ')')
+            k++;
+    if (k != 1)
+        return 0;
+
+    return 1;
+}
+
 int main()
 {
     cout << "USE FORMAT UNDER LINE\n---------------\n figure(x.y,r)\n";
     while (1) {
         bool InputCorrect = 0;
-
-        while (!InputCorrect) {
-            string input;
-            cin >> input;
-            if ("circle" == input.substr(0, input.find_first_of("("))) {
-                int xx = atoi(input.substr(input.find_first_of("(") + 1,
-                                           input.find_first_of(" ") - 1)
-                                      .c_str());
-                x.push_back(xx);
-                int yy = atoi(input.substr(input.find_first_of(".") + 1,
-                                           input.find_first_of(",") - 1)
-                                      .c_str());
-                y.push_back(yy);
-                int rad = atoi(input.substr(input.find_first_of(",") + 1,
-                                            input.find_last_of(")") - 1)
-                                       .c_str());
-                r.push_back(rad);
-            } else {
-                cout << "WRONG FORMAT";
-            }
+        string input;
+        cin >> input;
+        if("end" == input.substr(0, 3)) break;
+        if (!isFormatCorrect(input)) {
+            cout << "WRONG FORMAT" << endl;
+            continue;
         }
-        return 0;
+        if("fin" == input.substr(0, 3)) break;
+
+        if ("circle" == input.substr(0, input.find_first_of("("))) {
+            int xx = atoi(input.substr(input.find_first_of("(") + 1,
+                                       input.find_first_of(" ") - 1)
+                                  .c_str());
+            x.push_back(xx);
+            int yy = atoi(input.substr(input.find_first_of(".") + 1,
+                                       input.find_first_of(",") - 1)
+                                  .c_str());
+            y.push_back(yy);
+            int rad = atoi(input.substr(input.find_first_of(",") + 1,
+                                        input.find_last_of(")") - 1)
+                                   .c_str());
+            r.push_back(rad);
+        } else {
+            cout << "WRONG FORMAT" << endl;
+        }
     }
+    return 0;
+}

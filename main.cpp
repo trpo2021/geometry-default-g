5252f144b0a8460e7d32f55de795e97990d9cb7c
DefaultID <arteam_101@mail.ru>
2021-02-21 16:46:03 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,6 +13,9 @@ bool isFormatCorrect(string str)
     if (str.empty())
         return 0;
 
+    if (strchr(str.c_str(), ' '))
+        return 0;
+
     int k = 0;
     for (auto i : str)
         if (i == '.')
@@ -41,6 +44,11 @@ bool isFormatCorrect(string str)
     if (k != 1)
         return 0;
 
+    if (str.find('.') > str.find(','))
+        return 0;
+
+    if ("circle" != str.substr(0, str.find_first_of("(")))
+        return 0;
     return 1;
 }
 
@@ -48,16 +56,14 @@ int main()
 {
     cout << "USE FORMAT UNDER LINE\n---------------\n figure(x.y,r)\n";
     while (1) {
-        bool InputCorrect = 0;
         string input;
         cin >> input;
-        if("end" == input.substr(0, 3)) break;
+        if ("end" == input.substr(0, 3))
+            break;
         if (!isFormatCorrect(input)) {
             cout << "WRONG FORMAT" << endl;
             continue;
         }
-        if("fin" == input.substr(0, 3)) break;
-
         if ("circle" == input.substr(0, input.find_first_of("("))) {
             int xx = atoi(input.substr(input.find_first_of("(") + 1,
                                        input.find_first_of(" ") - 1)
@@ -71,8 +77,6 @@ int main()
                                         input.find_last_of(")") - 1)
                                    .c_str());
             r.push_back(rad);
-        } else {
-            cout << "WRONG FORMAT" << endl;
         }
     }
     return 0;
